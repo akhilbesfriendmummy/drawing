79fdacb067be89227247709158511f8d03edbfa1
akhilbesfriendmummy <74089296+akhilbesfriendmummy@users.noreply.github.com>
2023-04-06 14:23:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    posenet=ml5.poseNet(video,modelLoaded)
+    posenet.on('pose',gotPoses);
+}
+function draw(){
+    background("grey")
+}
+function modelLoaded(){
+    console.log("posenet is initialised");
+}
+function gotPoses(results){
+if (results.length>0){
+console.log(results);
+}
+}
