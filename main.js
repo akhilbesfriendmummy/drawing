204fdbbf2b008b1748709ef55790db0aeb6050a1
akhilbesfriendmummy <74089296+akhilbesfriendmummy@users.noreply.github.com>
2023-04-08 11:51:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
+
+
+
+
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -9,6 +20,10 @@ function setup(){
 }
 function draw(){
     background("grey")
+    fill("aqua");
+    stroke("red");
+    square(noseX,noseY,difference);
+    document.getElementById("square_side").innerHTML="width and height of square = "+difference+"px";
 }
 function modelLoaded(){
     console.log("posenet is initialised");
@@ -16,5 +31,11 @@ function modelLoaded(){
 function gotPoses(results){
 if (results.length>0){
 console.log(results);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+leftWristX=results[0].pose.leftWrist.x;
+rightWristX=results[0].pose.rightWrist.x;
+difference=floor(leftWristX-rightWristX);
+
 }
 }
